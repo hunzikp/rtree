1896fc983c6c8e9a3c726ce3f56761314714314e
Kent Johnson <kjohnson@akoyabio.com>
2021-05-04 13:35:00 -0400

    Add some examples

--- R/main.R
+++ R/main.R
@@ -4,8 +4,10 @@ Rcpp::loadModule("rtreecpp", TRUE)
 #'
 #' Organizes points in an R-tree.
 #'
-#' The R-tree is created using the quadratic splitting algorithm, with the maximum number of elements
-#' per node set to 16. See \url{http://www.boost.org/doc/libs/1_63_0/libs/geometry/doc/html/geometry/spatial_indexes/introduction.html} for details.
+#' The R-tree is created using the quadratic splitting algorithm,
+#' with the maximum number of elements
+#' per node set to 16.
+#' See \url{http://www.boost.org/doc/libs/1_63_0/libs/geometry/doc/html/geometry/spatial_indexes/introduction.html} for details.
 #'
 #' @param x A 2-column numeric matrix of point coordinates.
 #'
@@ -32,13 +34,30 @@ RTree <- function(x) {
 
 #' Get Points Within Distance
 #'
-#' For each point \eqn{y_i} in set \code{y}, returns the row-indices of the points indexed in \code{rTree}
+#' For each point \eqn{y_i} in set \code{y}, returns the row-indices
+#' of the points indexed in \code{rTree}
 #' that are within a given \code{distance} of \eqn{y_i}.
 #'
 #' @param rTree An \link{RTree} object.
 #' @param y A 2-column numeric matrix of point coordinates.
 #' @param distance A positive scalar.
+#' @return A list with one entry per row of `y`.
+#' Each entry is a vector of row indices.
+#' @examples
+#' # For two sets of points, A and B, find points in B within
+#' # a distance of 0.05 to points in A.
+#' n <- 10^4
+#' A <- cbind(runif(n), runif(n))
+#' B <- cbind(runif(n), runif(n))
 #'
+#' # Make an RTree on B
+#' B_rtree <- RTree(B)
+#'
+#' # Find points of B within 0.05 of each point in A
+#' nearby <- withinDistance(B_rtree, A, 0.05)
+#'
+#' # Each element of nearby is a list of row indices in B
+#' print(nearby[[1]])
 #' @export
 withinDistance <- function(rTree, y, distance) {
   UseMethod("withinDistance", rTree)
@@ -81,6 +100,21 @@ withinDistance.RTree <- function(rTree, y, distance) {
 #' @param y A 2-column numeric matrix of point coordinates.
 #' @param distance A positive scalar.
 #' @return An integer vector of with one entry for each row of `y`.
+#' @examples
+#' # For two sets of points, A and B, count points in B within
+#' # a distance of 0.05 to points in A.
+#' n <- 10^4
+#' A <- cbind(runif(n), runif(n))
+#' B <- cbind(runif(n), runif(n))
+#'
+#' # Make an RTree on B
+#' B_rtree <- RTree(B)
+#'
+#' # Find points of B within 0.05 of each point in A
+#' counts <- countWithinDistance(B_rtree, A, 0.05)
+#'
+#' # Each element of counts is a count of points in B
+#' print(counts[[1]])
 #' @export
 countWithinDistance <- function(rTree, y, distance) {
   UseMethod("countWithinDistance", rTree)
@@ -114,14 +148,30 @@ countWithinDistance.RTree <- function(rTree, y, distance) {
 
 #' Get Nearest Neighbors
 #'
-#' For each point \eqn{y_i} in set \code{y}, returns the row-indices of the \code{k} points indexed in \code{rTree}
+#' For each point \eqn{y_i} in set \code{y}, returns the row-indices of
+#' the \code{k} points indexed in \code{rTree}
 #' that are closest to \eqn{y_i}.
 #'
 #' @param rTree An \link{RTree} object.
 #' @param y A 2-column numeric matrix of point coordinates.
 #' @param k A positive integer.
-#' @return A list with one entry per row of `y`. Each entry is a vector of
-#' row indices.
+#' @return A list with one entry per row of `y`.
+#' Each entry is a vector of row indices.
+#' @examples
+#' # For two sets of points, A and B, find the 10 nearest neighbors in B
+#' # for each point in A.
+#' n <- 10^4
+#' A <- cbind(runif(n), runif(n))
+#' B <- cbind(runif(n), runif(n))
+#'
+#' # Make an RTree on B
+#' B_rtree <- RTree(B)
+#'
+#' # Find 10 nearest neighbors in B for each point in A
+#' nearest <- knn(B_rtree, A, 10L)
+#'
+#' # Each element of nearest is a vector of row indices of 10 points in B
+#' print(nearest[[1]])
 #' @export
 knn <- function(rTree, y, k) {
   UseMethod("knn", rTree)
